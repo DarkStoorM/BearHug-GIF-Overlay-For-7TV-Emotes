bb752e5d72b693d9f0381de3f22729e88d7809de
DarkStoorM <b.sieprawski@op.pl>
2022-12-31 19:55:04 +0100

    Use `html` tag for drop zone, add error reporting in the DOM

--- src/DropZone.ts
+++ src/DropZone.ts
@@ -1,13 +1,16 @@
 import { ImageProcessor } from "./ImageProcessor";
+import { ErrorMessage } from "./helpers/error-messages";
 import { EComponentIDs } from "./types/Enums/EComponentIDs";
 import { EOverlayState } from "./types/Enums/EOverlayState";
+import { Fader } from "./utils/Fader";
 
 export class DropZone {
+  private errorsContainer: HTMLDivElement;
   /**
    * Initial drop zone, which will trigger the page overlay. This part is required as the overlay
    * is initially disabled and dragging over the body will trigger the
    */
-  private htmlBody: HTMLBodyElement;
+  private htmlBody: HTMLHtmlElement;
   /**
    * Page overlay where users drop the file
    */
@@ -21,24 +24,33 @@ export class DropZone {
   public constructor() {
     // Fetch the required elements
     // No need to check them, because they exist in the DOM anyway ¯\_(ツ)_/¯
+    this.errorsContainer = document.getElementById(EComponentIDs.ERRORS_CONTAINER) as HTMLDivElement;
     this.htmlDropZone = document.getElementById(EComponentIDs.DROP_ZONE) as HTMLDivElement;
     this.htmlResultImage = document.getElementById(EComponentIDs.RESULT_IMAGE) as HTMLImageElement;
-    this.htmlBody = document.getElementsByTagName("body")[0] as HTMLBodyElement;
+    this.htmlBody = document.getElementsByTagName("html")[0] as HTMLHtmlElement;
 
     this.registerListeners();
   }
 
   /**
-   * Reenables the dropping functionality after the image processor is done
+   * Reenables the dropping functionality after the image processor is done and 'prints' the result image
+   * back to the user in the DOM
    *
    * @param   {string}  result  Result image that ultimately came back from the `ImageProcessor`
    */
   public processorFinish(result: string): void {
+    this.processorReenableDrop();
+
+    this.htmlResultImage.src = result;
+  }
+
+  /**
+   * Enables the dropping functionality
+   */
+  private processorReenableDrop(): void {
     this.isCurrentlyProcessing = false;
 
     this.overlayHide(EOverlayState.OVERLAY_WORKING);
-
-    this.htmlResultImage.src = result;
   }
 
   /**
@@ -130,10 +142,31 @@ export class DropZone {
       // Only pick the first file in case the user drops multiples, at least for now
       new ImageProcessor(e.dataTransfer.files[0]);
     } catch (error: unknown) {
-      // TODO: throw this as a UI error later
-      console.error(error);
-      throw new Error();
+      // Immediately allow dropping another image as there was no processing left
+      this.processorReenableDrop();
+
+      if (!(error instanceof ErrorMessage)) {
+        return this.triggerErrorMessage(new ErrorMessage("unknownError").message);
+      }
+
+      // "Throw" the error inside the error container in the DOM
+      this.triggerErrorMessage(error.message);
+      this.errorsContainer.innerHTML;
     }
+  }
+
+  /**
+   * Displays the given error message and fades out the Errors container to not remain in the DOM
+   * all the time
+   *
+   * @param   {string}  message  Error message to display in the container
+   */
+  private triggerErrorMessage(message: string): void {
+    this.errorsContainer.style.display = "block";
+    this.errorsContainer.style.opacity = "1";
+    this.errorsContainer.innerText = message;
+
+    Fader.fadeOut(this.errorsContainer, 1000, 3000);
   }
 
   private registerListeners(): void {
