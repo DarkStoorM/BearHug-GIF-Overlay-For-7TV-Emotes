b78b7ed0c5b65a6b82b1ef73b3d9933f97d96840
DarkStoorM <b.sieprawski@op.pl>
2022-12-31 19:56:02 +0100

    Implement custom errors class

--- src/helpers/error-messages.ts
+++ src/helpers/error-messages.ts
@@ -1,3 +1,16 @@
-export const ErrorMessages = {
+/**
+ * Used to throw `known` error messages within the application
+ */
+export class ErrorMessage extends Error {
+  public constructor(message: keyof typeof ErrorMessages) {
+    super(ErrorMessages[message]);
+
+    Object.setPrototypeOf(this, ErrorMessage.prototype);
+  }
+}
+
+const ErrorMessages = {
   unsupportedType: "Only GIF, PNG, JPEG, and WEBP file types are supported.",
+  unknownError:
+    "An unexpected error has occurred. You most likely used the recently generated image, which sometimes will get rejected by the browser.",
 };
--- src/types/Enums/EComponentIDs.ts
+++ src/types/Enums/EComponentIDs.ts
@@ -2,4 +2,5 @@ export enum EComponentIDs {
   DROP_ZONE = "drop-zone",
   MAIN_CONTAINER = "container",
   RESULT_IMAGE = "result-image",
+  ERRORS_CONTAINER = "errors-container",
 }
